a2cc7a26c5f3d97d9a3e472032f51692813a4257
Swanand Buva <destro1108@users.noreply.github.com>
2020-04-16 20:25:18 +0530

    DispatchSpelling Correction

--- src/components/auth/ProtectedRoutes.js
+++ src/components/auth/ProtectedRoutes.js
@@ -4,7 +4,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { showSnackbar } from '../../store/actions/uiActions'
 
 export const ProtectedRoutes = ({ component: Component, ...rest }) => {
-    const dispacth = useDispatch();
+    const dispatch = useDispatch();
     const { auth } = useSelector(
         state => state.firebase
     )
@@ -14,7 +14,7 @@ export const ProtectedRoutes = ({ component: Component, ...rest }) => {
             (props) => {
                 if(auth.uid) return <Component {...props}/>
                 else {
-                    dispacth(showSnackbar({variant: 'info', message: "Sign-in to continue!"}))
+                    dispatch(showSnackbar({variant: 'info', message: "Sign-in to continue!"}))
                     return <Redirect to={
                         {
                             pathname: '/signin',
